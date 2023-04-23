72f1d7130ae915087c03ad0f3dccec68367caec2
酷学 <kuxue@yanmonet.com>
2023-04-23 18:26:01 +0800

    0

--- Packages/NetSync/Runtime/Discovery/NetworkDiscovery.cs
+++ Packages/NetSync/Runtime/Discovery/NetworkDiscovery.cs
@@ -5,7 +5,6 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 namespace Yanmonet.NetSync
